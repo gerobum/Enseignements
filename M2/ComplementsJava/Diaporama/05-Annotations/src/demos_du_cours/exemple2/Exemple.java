32ce8939a5df4f66b579152cffedef5d3b7b92fb
Yvan <yvan.maillot@uha.fr>
2016-11-08 11:20:09 +0100

    M2 - Complément Java
    Démonstrations du cours sur les annotations
    
    Type d'annotations PreambuleClasse contenant une propriété de type type d'annotations.

---
+++ M2/ComplementsJava/Diaporama/05-Annotations/src/demos_du_cours/exemple2/Exemple.java
@@ -0,0 +1,16 @@
+package demos_du_cours.exemple2;
+
+
+/**
+ *
+ * @author maillot
+ */
+@PreambuleClasse(
+        auteur = "Yvan",
+        creation = "09/05/2016",
+        revision = "09/11/2016",
+        version = @Version(mineur = 2)
+)
+public class Exemple {
+
+}
