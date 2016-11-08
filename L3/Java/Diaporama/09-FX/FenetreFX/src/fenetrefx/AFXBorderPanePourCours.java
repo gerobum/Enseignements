28ddfbcaaa4b32f2bea8b4548aa8124247ee15c6
Yvan <yvan.maillot@uha.fr>
2016-11-08 11:20:09 +0100

    L2 - Java
    Démonstrations du cours sur JavaFx
    
    Démo BorderPane
    Ajout d'un peu de couleur

--- L3/Java/Diaporama/09-FX/FenetreFX/src/fenetrefx/AFXBorderPanePourCours.java
+++ L3/Java/Diaporama/09-FX/FenetreFX/src/fenetrefx/AFXBorderPanePourCours.java
@@ -5,6 +5,8 @@ import static javafx.application.Application.launch;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
@@ -12,6 +14,7 @@ import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -25,26 +28,36 @@ public class AFXBorderPanePourCours extends Application {
     private final Border borderLR = new Border(new BorderStroke(null, null, null, null,
             BorderStrokeStyle.NONE, BorderStrokeStyle.DOTTED, BorderStrokeStyle.NONE, BorderStrokeStyle.DOTTED,
             CornerRadii.EMPTY, new BorderWidths(1.0), Insets.EMPTY));
+    private final Background rouge = new Background(new BackgroundFill(Color.rgb(255, 200, 200), null, null));
+    private final Background vert = new Background(new BackgroundFill(Color.rgb(200, 255, 200), null, null));
+    private final Background bleu = new Background(new BackgroundFill(Color.rgb(200, 200, 255), null, null));
+    private final Background jaune = new Background(new BackgroundFill(Color.rgb(255, 255, 200), null, null));
+    private final Background cyan = new Background(new BackgroundFill(Color.rgb(200, 255, 255), null, null));
 
     @Override
     public void start(Stage primaryStage) {
         top = new StackPane(new Label("Top (0x50)"));
         top.setPrefSize(50, 50);
         top.setBorder(borderAll);
+        top.setBackground(rouge);
 
         center = new StackPane(new Label("Center (200x200)"));
         center.setPrefSize(200, 200);
+        center.setBackground(vert);
 
         bottom = new StackPane(new Label("Bottom (30x30)"));
         bottom.setPrefSize(30, 30);
         bottom.setBorder(borderAll);
+        bottom.setBackground(bleu);
 
         left = new StackPane(new Label(" Left "));
         left.setBorder(borderLR);
+        left.setBackground(jaune);
 
         right = new StackPane(new Label(" Right "));
         right.setBorder(borderLR);
-
+        right.setBackground(cyan);
+        
         BorderPane pane = new BorderPane();
         pane.setTop(top);
         pane.setCenter(center);
