be304838a591a3ea28ddbd1b022137903cc191a8
Yvan <yvan.maillot@uha.fr>
2016-11-08 11:20:09 +0100

    L3 - Java
    Démonstrations du cours sur JavaFx

---
+++ L3/Java/Diaporama/09-FX/FenetreFX/src/fenetrefx/ApplicationFX.java
@@ -0,0 +1,48 @@
+package fenetrefx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author yvan
+ */
+public class ApplicationFX extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {     
+        primaryStage.initStyle(StageStyle.DECORATED);
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Une fenêtre qui fait de l'effet");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
