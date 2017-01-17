efbafa40dc576c96d81512b52beed71496657c74
Yvan <yvan.maillot@uha.fr>
2017-01-17 11:25:26 +0100

    L3 Java Cours sur les classes
    Explication de l'intérêt des classes non-mutables avec FX

--- L3/Java/Diaporama/03-Les-classes/Pourquoi-Non-Mutable-FX/src/main/Main.java
+++ L3/Java/Diaporama/03-Les-classes/Pourquoi-Non-Mutable-FX/src/main/Main.java
@@ -1,6 +1,11 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,9 +13,43 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Intérêt des classes non-mutables");
-        
+        Group root = new Group();
+        Canvas canvas = new Canvas(300, 250);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawShapes(gc);
+        root.getChildren().add(canvas);
+        stage.setScene(new Scene(root));
+        stage.show();
     }
     
+    private void drawPoint(GraphicsContext gc, double x, double y) {
+        gc.fillOval(x-5, y-5, y+5, y+5);
+    }
+
+    private void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        drawPoint(gc, 50, 50);
+        /*gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeOval(60, 60, 30, 30);
+        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+        gc.fillPolygon(new double[]{10, 40, 10, 40},
+                new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolygon(new double[]{60, 90, 60, 90},
+                new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolyline(new double[]{110, 140, 110, 140},
+                new double[]{210, 210, 240, 240}, 4);*/
+    }
+
     public static void main(String[] args) {
         launch();
     }
