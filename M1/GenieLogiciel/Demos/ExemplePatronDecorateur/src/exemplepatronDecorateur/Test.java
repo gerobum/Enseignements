fd9391082640debaa87898b8a1b6a870f2bbaccc
Yvan <yvan.maillot@uha.fr>
2018-02-19 18:02:51 +0100

    Des modifications mineurs

--- M1/GenieLogiciel/Demos/ExemplePatronDecorateur/src/exemplepatronDecorateur/Test.java
+++ M1/GenieLogiciel/Demos/ExemplePatronDecorateur/src/exemplepatronDecorateur/Test.java
@@ -1,5 +1,7 @@
 package exemplepatronDecorateur;
+
 public class Test {
+
     public static void main(String[] args) {
         Composant c1 = new ComposantConcret();
         System.out.println(c1);
@@ -9,17 +11,6 @@ public class Test {
         System.out.println(c3);
         Composant c4 = new DecorateurConcretA(c3);
         System.out.println(c4);
-        int v = 0, vn = 0;
-        int d = 0;
-        for(int i = 1; i < 35; ++i) {
-            vn = (int) Math.round(i/35.0*8);
-            if (vn != v) {
-                System.out.println(String.format("Entre %d et %d \t-> %d", d, i-1, v));
-                v = vn;
-                d = i;
-            }
-        }
-        System.out.println(String.format("Entre %d et %d \t-> %d", d, 37, 8));
     }
 
 }
