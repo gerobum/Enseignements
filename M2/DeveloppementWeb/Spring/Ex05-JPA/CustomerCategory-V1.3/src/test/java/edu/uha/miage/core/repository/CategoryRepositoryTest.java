225875ef8594d28f59be52d2ae11bcb88bfb536b
Yvan Maillot <yvan.maillot@uha.fr>
2022-10-31 23:18:45 +0100

    Refresh CCV1.3

--- M2/DeveloppementWeb/Spring/Ex05-JPA/CustomerCategory-V1.3/src/test/java/edu/uha/miage/core/repository/CategoryRepositoryTest.java
+++ M2/DeveloppementWeb/Spring/Ex05-JPA/CustomerCategory-V1.3/src/test/java/edu/uha/miage/core/repository/CategoryRepositoryTest.java
@@ -1,45 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.uha.miage.core.repository;
 
 import edu.uha.miage.core.entity.Category;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
-/**
- *
- * @author yvan
- */
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
 public class CategoryRepositoryTest {
-    // #### V1.2  Du logging peut être utile
     private static final Logger LOGGER = LoggerFactory.getLogger(CategoryRepositoryTest.class);
-    // #### V1.2 Du logging peut être utile
-    
-    // #### V1.2 Injection de dépendance 
+
     @Autowired
     CategoryRepository categoryRepository;
     
 
-
-    // #### V1.2 Des exemples sont enregistrés avant chaque test. 
     @BeforeEach
     public void setUp() {
         saveCategory("A");
@@ -54,7 +36,6 @@ public class CategoryRepositoryTest {
         }
     }
 
-    // #### V1.2 Test pour voir si le findByName se passe bien.
     @Test
     public void testFindByName() {
         LOGGER.info("Test findByName");
@@ -65,7 +46,6 @@ public class CategoryRepositoryTest {
         assertNotNull(categoryRepository.findByName("W"));
     }
 
-    // #### V1.2 Et ainsi de suite. Voir un cours sur les tests unitaires.
     @Test
     public void testCount() {
         LOGGER.info("Test Count");
@@ -75,7 +55,6 @@ public class CategoryRepositoryTest {
         assertEquals(4, categoryRepository.count());
     }
 
-    // #### V1.2 Ce ne sont que des exemples. 
     @Test
     public void testDelete() {
         LOGGER.info("Test Count");
