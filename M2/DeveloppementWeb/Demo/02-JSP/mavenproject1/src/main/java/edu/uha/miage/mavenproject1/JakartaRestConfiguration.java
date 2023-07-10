875a70edc2bd42a7abd7c0e400d458a13ea32717
Yvan Maillot <yvan.maillot@uha.fr>
2023-07-10 19:52:16 +0200

    M2

---
+++ M2/DeveloppementWeb/Demo/02-JSP/mavenproject1/src/main/java/edu/uha/miage/mavenproject1/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package edu.uha.miage.mavenproject1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
